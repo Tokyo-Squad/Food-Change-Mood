571c2a4a6b90f2e132f41d23aa3e129917cc8362
Mostafa Mohamed <Mostafa0Mohamed2002@gmail.com>
2025-04-14 20:09:11 +0200

    add Meal , Nutrition data model

---
+++ src/main/kotlin/model/Meal.kt
@@ -0,0 +1,26 @@
+package org.example.model
+
+data class Meal(
+    val name: String,
+    val id: Int,
+    val preparationTime: Int,
+    val contributorId: Int,
+    val submitted: String,
+    val tags: List<String>,
+    val nutrition: Nutrition,
+    val numberOfSteps: Int,
+    val steps: List<String>,
+    val description: String?,
+    val ingredients: List<String>,
+    val numberOfIngredients: Int
+)
+
+data class Nutrition(
+    val calories: Double,
+    val totalFat: Double,
+    val sugar: Double,
+    val sodium: Double,
+    val protein: Double,
+    val saturatedFat: Double,
+    val carbohydrates: Double
+)
