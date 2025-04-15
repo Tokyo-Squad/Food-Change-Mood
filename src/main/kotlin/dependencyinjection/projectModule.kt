e9e6342ccf5f9f5dd6d7e0d19894502b11ef3def
Hasan Ali <ihasanalihasan@gmail.com>
2025-04-16 00:44:16 +0300

    inject keto diet meal helper use cse in project module

--- src/main/kotlin/dependencyinjection/projectModule.kt
+++ src/main/kotlin/dependencyinjection/projectModule.kt
@@ -4,6 +4,7 @@ import org.example.data.CsvMealParser
 import org.example.data.CsvMealReader
 import org.example.data.CsvRepositoryImpl
 import org.example.logic.CsvRepository
+import org.example.logic.KetoDietMealHelperUseCase
 import org.koin.dsl.module
 
 val projectModule = module {
@@ -12,4 +13,6 @@ val projectModule = module {
     single { CsvMealParser() }
 
     single<CsvRepository> { CsvRepositoryImpl(get(), get()) }
+
+    single { KetoDietMealHelperUseCase(csvRepository = get()) }
 }
