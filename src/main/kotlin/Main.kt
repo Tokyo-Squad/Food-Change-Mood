0e07a6fa537d861392e7205644e62fa5586ee55c
mohamedebrahem13 <mohamedebrahem1447@gmail.com>
2025-04-16 00:24:23 +0200

    remove main example code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,14 +2,10 @@ package org.example
 
 import dependencyinjection.projectModule
 import org.example.dependencyinjection.useCaseModule
-import org.example.logic.CsvRepository
 import org.koin.core.context.GlobalContext.startKoin
-import org.koin.mp.KoinPlatform.getKoin
 
 fun main() {
     startKoin {
         modules(projectModule,useCaseModule)
     }
-    val repository: CsvRepository = getKoin().get()
-    repository.getMeals().forEach { println(it) }
 }
