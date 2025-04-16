c0270fc693b9b471efbcebebc9b2e23a0aee391e
Hasan Ali <ihasanalihasan@gmail.com>
2025-04-16 23:45:56 +0300

    add throw exception  to the get large group meal use case

--- src/main/kotlin/logic/GetLargeGroupMealUseCase.kt
+++ src/main/kotlin/logic/GetLargeGroupMealUseCase.kt
@@ -1,18 +1,28 @@
 package org.example.logic
 
 import org.example.model.Meal
+import org.example.utils.MealNotFoundException
 import org.example.utils.containAnyOf
 
 class GetLargeGroupItalyMealUseCase(
     private val csvRepository: CsvRepository
 ) {
 
-    fun getLargeGroupItalyMeal(): List<Meal>{
+    fun getLargeGroupItalyMeal(): List<Meal> {
         return getItalyMeals().filter { it.tags.contains("for-large-groups") }
+            .takeIf { it.isNotEmpty() }
+            ?: throw MealNotFoundException("No meals found for large groups")
     }
 
-    private fun getItalyMeals(): List<Meal> =  getAllMeals().filter { it.containAnyOf("italy") }
+    private fun getItalyMeals(): List<Meal> = getAllMeals()
+        .filter {
+            it.containAnyOf("italy") ||
+                    it.containAnyOf("italian")
+        }
+        .takeIf { it.isNotEmpty() }
+        ?: throw MealNotFoundException("No meals found for large groups")
 
-    private fun getAllMeals(): List<Meal> = csvRepository.getMeals()
+    private fun getAllMeals(): List<Meal> = csvRepository.getMeals().takeIf { it.isNotEmpty() }
+        ?: throw MealNotFoundException("No meals found for large groups")
 
 }
