a755fb8138989ec0670ef6a1c496fb68f8ccd059
Hasan Ali <ihasanalihasan@gmail.com>
2025-04-17 14:06:32 +0300

    change name function to invoke and use containAnyOf and use randomElementUnique

--- src/main/kotlin/logic/ExploreCountriesFoodCultureUseCase.kt
+++ src/main/kotlin/logic/ExploreCountriesFoodCultureUseCase.kt
@@ -1,22 +1,19 @@
 package org.example.logic
 
 import org.example.model.Meal
+import org.example.utils.containAnyOf
+import org.example.utils.randomElementsUnique
 
 class ExploreCountriesFoodCultureUseCase(
     private val csvRepository: CsvRepository,
 ) {
 
-    fun findMealsByCountry(country: String): List<Meal> {
+    fun invoke(country: String): List<Meal> {
         val normalizedCountry = country.lowercase().trim()
 
         return csvRepository.getMeals()
             .filter { meal ->
-                meal.name.contains(normalizedCountry, ignoreCase = true) ||
-                        meal.tags.any { it.contains(normalizedCountry, ignoreCase = true) } ||
-                        meal.description?.contains(normalizedCountry, ignoreCase = true) ?: false ||
-                        meal.ingredients.any { it.contains(normalizedCountry, ignoreCase = true) }
-            }
-            .shuffled()
-            .take(20)
+                meal.containAnyOf(normalizedCountry)
+            }.randomElementsUnique(20)
     }
 }
