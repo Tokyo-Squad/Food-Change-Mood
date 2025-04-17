cbf10e6861c12aedf8c8953d856267079649a8e9
Hasan Ali <ihasanalihasan@gmail.com>
2025-04-17 14:25:22 +0300

    add getGymHelper to console

--- src/main/kotlin/presentation/ConsoleUi.kt
+++ src/main/kotlin/presentation/ConsoleUi.kt
@@ -2,6 +2,7 @@ package org.example.presentation
 
 import org.example.logic.GetMealByIdUseCase
 import org.example.logic.GetMealsByAddDateUseCase
+import org.example.logic.GymHelperUseCase
 
 fun getMealsByDateConsole(getMealsByAddDateUseCase: GetMealsByAddDateUseCase) {
     println("Enter a date (yyyy-MM-dd) to search for meals:")
@@ -32,4 +33,22 @@ fun getMealByIdConsole(getMealByIdUseCase: GetMealByIdUseCase) {
     } else {
         println("Invalid input. Please enter a valid meal ID.")
     }
+}
+
+fun getGymHelper(gymHelper: GymHelperUseCase){
+    print("please enter your target calories: ")
+    val targetCalories = readln().toFloatOrNull()
+    print("please enter your target calories: ")
+    val targetProtein = readln().toFloatOrNull()
+
+    if(targetCalories != null && targetProtein != null ){
+        gymHelper.invoke(targetCalories = targetCalories, targetProtein = targetProtein).forEachIndexed { index, meal ->
+            println("${index + 1} - $meal")
+        }
+    }
+    else
+        throw NullPointerException("please enter correct value")
+
+
+
 }
