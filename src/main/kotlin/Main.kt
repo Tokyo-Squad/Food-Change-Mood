47e6bac89096e68d875bdfbf1dd03669e57db930
Mostafa Mohamed Fahmy <MostafaMohamedFahmy1st@gmail.com>
2025-04-18 10:22:45 +0200

    implement the application Interface

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,17 @@
 package org.example
 
 import dependencyinjection.projectModule
+import kotlinx.datetime.Clock
+import org.example.data.CsvMealReader
 import org.example.dependencyinjection.useCaseModule
+import org.example.presentation.ConsoleUi
 import org.koin.core.context.GlobalContext.startKoin
+import org.koin.java.KoinJavaComponent
 
 fun main() {
     startKoin {
-        modules(projectModule,useCaseModule)
+        modules(projectModule, useCaseModule)
     }
+    val ui: ConsoleUi = KoinJavaComponent.getKoin().get()
+    ui.start()
 }
--- src/main/kotlin/dependencyinjection/projectModule.kt
+++ src/main/kotlin/dependencyinjection/projectModule.kt
@@ -4,23 +4,12 @@ import org.example.data.CsvMealParser
 import org.example.data.CsvMealReader
 import org.example.data.CsvRepositoryImpl
 import org.example.logic.CsvRepository
-import org.example.logic.KetoDietMealHelperUseCase
-import org.example.logic.GetMealByIdUseCase
-import org.example.logic.GetMealsByAddDateUseCase
-import org.example.logic.SweetMealWithoutEggUseCase
+import org.example.presentation.ConsoleUi
 import org.koin.dsl.module
 
 val projectModule = module {
     single { CsvMealReader("food.csv") }
-
     single { CsvMealParser() }
-
     single<CsvRepository> { CsvRepositoryImpl(get(), get()) }
-
-    single { KetoDietMealHelperUseCase(csvRepository = get()) }
-
-    single { SweetMealWithoutEggUseCase(csvRepository = get()) }
-    
-    single { EasyFoodSuggestionUseCase(get()) }
-
+    single { ConsoleUi() }
 }
