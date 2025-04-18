c2a4c38548fbdb52cddd7c1b1a4a2dbd5d79c0a3
Kareem Mohamed <info.kareemmohamed@gmail.com>
2025-04-18 21:21:09 +0200

    inject repository interface into use case.

--- src/main/kotlin/logic/GetRandomPotatoMealsUseCase.kt
+++ src/main/kotlin/logic/GetRandomPotatoMealsUseCase.kt
@@ -3,14 +3,13 @@ package org.example.logic
 import org.example.model.Meal
 import org.example.utils.randomElementsUnique
 
-class GetRandomPotatoMealsUseCase {
-    operator fun invoke(meals: List<Meal>, randomSize: Int = 10): List<Meal> =
-        meals.filter { meal ->
+class GetRandomPotatoMealsUseCase(
+    private val csvRepository: CsvRepository
+) {
+    operator fun invoke(randomSize: Int = 10): List<Meal> =
+        csvRepository.getMeals().filter { meal ->
             meal.ingredients.any { ingredient ->
-                ingredient.contains("potato", ignoreCase = true) || ingredient.contains(
-                    "potatoes",
-                    ignoreCase = true
-                )
+                ingredient.contains("potato", ignoreCase = true) || ingredient.contains("potatoes", ignoreCase = true)
             }
-        }.toList().randomElementsUnique(randomSize)
+        }.randomElementsUnique(randomSize)
 }
