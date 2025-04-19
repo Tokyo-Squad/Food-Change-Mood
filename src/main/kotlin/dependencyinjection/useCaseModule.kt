5d3f26307b83a0e0069f6ef2ad2ff5b6a73e7f0d
Hasan Ali <ihasanalihasan@gmail.com>
2025-04-19 14:25:24 +0300

    inject GetRandomMealUseCase to koin module

--- src/main/kotlin/dependencyinjection/useCaseModule.kt
+++ src/main/kotlin/dependencyinjection/useCaseModule.kt
@@ -11,6 +11,7 @@ import org.example.logic.HighCalorieMealSuggestionUseCase
 val useCaseModule = module {
     single { GetHealthyFastFoodMealsUseCase(get<CsvRepository>()) }
     single { GetMealsByAddDateUseCase(get()) }
+    single { GetRandomMealUseCase(csvRepository = get()) }
     single { GetSweetMealUseCase(csvRepository = get()) }
     single { GetSweetMealsNotContainEggUseCase(sweetMeal = get()) }
     single { SweetMealWithoutEggUseCase(csvRepository = get(), sweetMealsNotContainEgg = get()) }
@@ -23,5 +24,7 @@ val useCaseModule = module {
     single { HighCalorieMealSuggestionUseCase(get()) }
     single { GetMealsByNameUseCase(get()) }
     single { GenerateIngredientQuestionUseCase(get()) }
+    single { GetRandomPotatoMealsUseCase(csvRepository = get()) }
+    single { GetSeafoodMealsSortedByProteinUseCase(csvRepository = get()) }
 
 }
